af41af0c7e73f3c68fc55f9c774307490177c309
Mon archlinux virtualbox <algassimoupelleldiallo2005@gmail.com>
2024-12-11 00:09:49 +0100

    finition de tp 9 et 8

--- SCR1.2/TP8/bin2dot-with-for.sh
+++ SCR1.2/TP8/bin2dot-with-for.sh
@@ -1,19 +1,47 @@
 #!/bin/bash
 
-if [[ $# -ne 2 ]]; then
+# Vérifier si deux arguments sont fournis
+if [ "$#" -ne 2 ]; then
     echo "Usage: $0 <SRC_FILE> <DEST_FILE>"
     exit 1
 fi
 
-#cp /dev/null $2
-for addr in $(cat $1); do
-    #statements
-    #On recupere le digits 
-    octet1=$(expr substr $addr 1 8)
-    octet2=$(expr substr $addr 9 8)
-    octet3=$(expr substr $addr 17 8)
-    octet4=$(expr substr $addr 25 8)
-    addr_dot="$((2#$octet1)).$((2#$octet2)).$((2#$octet3)).$((2#$octet4))"
-    echo $addr_dot >> $2
-done 
-exit 0 
+SRC_FILE=$1
+DEST_FILE=$2
+
+# Vérifier si le fichier de destination existe déjà
+if [ -f "$DEST_FILE" ]; then
+    echo "$DEST_FILE existe déjà. Voulez-vous le remplacer ? (o/n)"
+    read answer
+    if [ "$answer" != "o" ]; then
+        echo "Annulation."
+        exit 0
+    fi
+fi
+
+# Vider le fichier de destination si nécessaire
+> "$DEST_FILE"
+
+i=1
+
+# Lire chaque ligne du fichier source
+cat "$SRC_FILE" | while read -r line; do
+    # Extraire les 4 octets de l'adresse binaire avec 'expr'
+    octet1=$(expr substr "$line" 1 8)
+    octet2=$(expr substr "$line" 9 8)
+    octet3=$(expr substr "$line" 17 8)
+    octet4=$(expr substr "$line" 25 8)
+
+    dec1=$((2#$octet1))
+    dec2=$((2#$octet2))
+    dec3=$((2#$octet3))
+    dec4=$((2#$octet4))
+
+    # Ajouter l'adresse IPv4 en notation décimale pointée au fichier de destination
+    echo "$i - $dec1.$dec2.$dec3.$dec4" >> "$DEST_FILE"
+
+    #incrementation du compteur i
+    i=$((i + 1))
+done
+
+echo "La conversion est terminée. Les adresses IPv4 sont dans $DEST_FILE."
